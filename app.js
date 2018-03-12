df1c4fe1f9e8f10cc3db01d3bd2be23f6ae56650
heyliddle <eric.liddle@gmail.com>
2018-03-12 16:30:37 -0700

    added single camp view which renders data from specific camp

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ mongoose.connect("mongodb://localhost/yelpcamp");
 //Schema:
 const campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 const Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //   name: "Harrison Hot Springs",
-//   image: "http://www.news1130.com/wp-content/blogs.dir/sites/9/2017/07/03/harrison-fire-e1499104828226.jpg"
+//   image: "http://www.news1130.com/wp-content/blogs.dir/sites/9/2017/07/03/harrison-fire-e1499104828226.jpg",
+//   description: "Harrison Hot Springs is a small town with a lake at the foot of the river.  Up a logging road you'll find pebble beach campgrounds."
 // }, (err, newcamp) => {
 //   if(err){
 //     console.log("Campground not created.")
@@ -26,7 +28,6 @@ const Campground = mongoose.model("Campground", campgroundSchema);
 //   }
 // });
 
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -35,29 +36,13 @@ app.get("/", (req, res) => {
   res.render("landing");
 });
 
-// let campgrounds = [
-//   { name: "Salmon Creek", image: "http://www.mbpost.com/images/original/271559.jpg" },
-//   { name: "Crystal Lake", image: "http://www.davessierrafishing.com/photos300/crystal_lake2.jpg" },
-//   { name: "Narin Falls", image: "https://scontent-ort2-1.cdninstagram.com/vp/31a5cc819e88d001d8be8a59ce2c6851/5B3ED071/t51.2885-15/e35/28435082_211679469604582_1009801280064847872_n.jpg" },
-//   { name: "Salmon Creek", image: "http://www.mbpost.com/images/original/271559.jpg" },
-//   { name: "Crystal Lake", image: "http://www.davessierrafishing.com/photos300/crystal_lake2.jpg" },
-//   { name: "Narin Falls", image: "https://scontent-ort2-1.cdninstagram.com/vp/31a5cc819e88d001d8be8a59ce2c6851/5B3ED071/t51.2885-15/e35/28435082_211679469604582_1009801280064847872_n.jpg" },
-//   { name: "Salmon Creek", image: "http://www.mbpost.com/images/original/271559.jpg" },
-//   { name: "Crystal Lake", image: "http://www.davessierrafishing.com/photos300/crystal_lake2.jpg" },
-//   { name: "Narin Falls", image: "https://scontent-ort2-1.cdninstagram.com/vp/31a5cc819e88d001d8be8a59ce2c6851/5B3ED071/t51.2885-15/e35/28435082_211679469604582_1009801280064847872_n.jpg" },
-//   { name: "Coachwood Camping", image: "http://www.findacamp.com.au/pictures/large_522.JPG" }
-// ];
-
-
-// Originally this just found the campgrounds in the array above.
-// Now campgrounds is pulled from mongo
 app.get("/campgrounds", (req, res) => {
   Campground.find({}, (err, campgrounds) => {
     if (err) {
       console.log("Cant find campgrounds");
       console.log(err);
     } else {
-      res.render("campgrounds", { campgrounds: campgrounds });
+      res.render("index", { campgrounds: campgrounds });
     }
   })
 });
@@ -65,7 +50,8 @@ app.get("/campgrounds", (req, res) => {
 app.post("/campgrounds", (req, res) => {
   let campName = req.body.name;
   let campImage = req.body.image;
-  let newCampground = { name: campName, image: campImage };
+  let campDesrcipton = req.body.description;
+  let newCampground = { name: campName, image: campImage, description: campDesrcipton };
   Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
       console.log(err);
@@ -74,9 +60,19 @@ app.post("/campgrounds", (req, res) => {
     }
   })
 });
-
 app.get("/campgrounds/new", (req, res) => {
   res.render("addcampground")
+});
+
+app.get("/campgrounds/:id", (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if(err) {
+      console.log(err);
+    } else {
+      req.params.id
+      res.render("show", {campground: foundCampground});
+    };
+  });
 });
 
 app.listen(3000, () => {
